bfa9ec73d1ac1e2ea415b4f66bfcc8f793f04e12
anilkharatmol <akharatmol8@gmail.com>
2025-04-24 16:53:53 +0530

    Routes added

--- FirstServer/src/app.js
+++ FirstServer/src/app.js
@@ -39,6 +39,24 @@ app.get("/welcome",(req,res)=>{
   res.send(`<h2>Welcome,${req.user}</h2>`)
 })
 
+
+app.get('/orders', (req, res) => {
+  res.send('Here is the list of all orders.');
+});
+
+app.post('/orders', (req, res) => {
+  res.send('A new order has been created.');
+});
+
+app.get('/users', (req, res) => {
+  res.send('Here is the list of all users.');
+});
+
+app.post('/users', (req, res) => {
+  res.send('A new user has been added.');
+});
+
+
 app.listen(PORT, () => {
   console.log(`Server is up and running on port ${PORT}! Ready to handle requests.`);
 });
